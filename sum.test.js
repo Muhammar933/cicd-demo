68f07a945bcc6f115d99bcb731998e873af5a7af
Muhammar933 <muhammar4567@gmail.com>
2026-09-20 15:03:11 +0500

    Add multiple feature

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,9 @@
-const sum = require('./sum');
+const { sum, multiply } = require('./sum');
 
 test('adds 2 + 3 to equal 5', () => {
   expect(sum(2, 3)).toBe(5);
+});
+
+test('multiplies 3 * 4 to equal 12', () => {
+  expect(multiply(3, 4)).toBe(12);
 });
